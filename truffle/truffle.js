24c7038597b889861cc0b14d93dffbe714451ca7
jpsiyu <jpsiyu@163.com>
2019-01-10 21:19:11 +0800

    change

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -9,14 +9,14 @@ module.exports = {
       host: "127.0.0.1", // Connect to geth on the specified
       port: 8545,
       network_id: 3,
-      gasPrice: 11000000000,
+      gasPrice: 21000000000,
       gas: 8003914,
     },
     rinkeby: {
       host: "127.0.0.1", // Connect to geth on the specified
       port: 8545,
       network_id: 4,
-      gasPrice: 11000000000,
+      gasPrice: 21000000000,
       gas: 8003914,
     }
   },
