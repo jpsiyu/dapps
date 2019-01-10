7eed1cfee93af1ab740b7b7c7a67e108e1a871a9
jpsiyu <jpsiyu@163.com>
2019-01-10 20:47:57 +0800

    change config

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -9,13 +9,11 @@ module.exports = {
       host: "127.0.0.1", // Connect to geth on the specified
       port: 8545,
       network_id: 3,
-      gas: 4612388 // Gas limit used for deploys
     },
     rinkeby: {
       host: "127.0.0.1", // Connect to geth on the specified
       port: 8545,
       network_id: 4,
-      gas: 4612388 // Gas limit used for deploys
     }
   },
   compilers: {
